a7ec899ee3dcd374e7500405343ee57ce8c40cc0
Andrey Volkov <[fullon1988@gmail.com]>
2020-09-16 14:59:53 +0300

    Updates

---
+++ precache-manifest.4f97104baf11acc84460b20def2ab9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5d354d75b5f3a9c395f2555590853b",
+    "url": "/react-components/index.html"
+  },
+  {
+    "revision": "7382fb46b5f2c1b5974b",
+    "url": "/react-components/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5a233d5f1c85b27f3b8e",
+    "url": "/react-components/static/js/2.a1545d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-components/static/js/2.a1545d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7382fb46b5f2c1b5974b",
+    "url": "/react-components/static/js/main.24f058ac.chunk.js"
+  },
+  {
+    "revision": "5da8705d3e59bb8e2421",
+    "url": "/react-components/static/js/runtime-main.d510f83f.js"
+  }
+]);
